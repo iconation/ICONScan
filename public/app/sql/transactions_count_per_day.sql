6a7dbf1489ae2a35116bebda7cb1db68547b691c
Spl3en <spl3en.contact@gmail.com>
2019-04-24 23:50:01 +0200

    Count the actual total transactions per day

--- public/app/sql/transactions_count_per_day.sql
+++ public/app/sql/transactions_count_per_day.sql
@@ -1,6 +1,18 @@
 SELECT 
     DATE(FROM_UNIXTIME(block.timestamp DIV 1000000)),
-    COUNT(DISTINCT id) totalCount
+    COUNT(DISTINCT all_transactions.txid) 
 FROM
     iconation.block
+INNER JOIN
+    (
+		SELECT
+			transaction.id as txid,
+			transaction.block as blockid
+		FROM iconation.transaction
+		LEFT JOIN
+			internal_transaction
+		ON
+			internal_transaction.transaction = iconation.transaction.id
+    ) as all_transactions
+    ON all_transactions.blockid = iconation.block.id
 GROUP BY DATE(FROM_UNIXTIME(block.timestamp DIV 1000000))
